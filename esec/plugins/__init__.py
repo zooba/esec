7f6c6ef5a0e751592bc200584b9290f012b3fba7
Steve Dower <s.j.dower@gmail.com>
2010-07-02 08:35:49 +1000

    Fixed licence for esec/plugins

--- esec/plugins/__init__.py
+++ esec/plugins/__init__.py
@@ -1,3 +1,3 @@
-#   Files contained in this directory (esec/cfgs) are not licenced
+#   Files contained in this directory (esec/plugins) are not licenced
 #   under the Apache License, Version 2.0, unless specified in the
 #   contents of the file.
