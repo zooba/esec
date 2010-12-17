a8ea5c81c6eecb44b00f1691997f8f2e410b27ce
Steve Dower <steve@stevedower.id.au>
2010-12-17 12:05:03 +1100

    Updated SSGA dialect to reduce births.

--- esec/dialects.py
+++ esec/dialects.py
@@ -181,12 +181,13 @@ YIELD population
 BEGIN generation
     REPEAT (size)
         FROM population SELECT 2 parents USING binary_tournament
-        FROM parents    SELECT offspring USING crossover_one(per_pair_rate=0.9), \
-                                               mutate_random(per_gene_rate=0.01)
+        FROM parents    SELECT 1 offspring \
+             USING crossover_one(per_pair_rate=0.9), \
+                   mutate_random(per_gene_rate=0.01), \
+                   best
         
-        FROM offspring      SELECT 1 replacer       USING best
-        FROM population     SELECT 1 replacee, rest USING uniform_shuffle
-        FROM replacer, rest SELECT population
+        FROM population      SELECT 1 replacee, rest USING uniform_shuffle
+        FROM offspring, rest SELECT population
     END repeat
 
     YIELD population
